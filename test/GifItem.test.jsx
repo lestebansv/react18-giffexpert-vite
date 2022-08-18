56f8be5553881d4326765f7e0f36ad6981b9a6ae
Esteban Sichacá <estebangee@hotmail.com>
2022-08-17 19:58:16 -0500

    Test Units in GifItem component

---
+++ test/GifItem.test.jsx
@@ -0,0 +1,28 @@
+import { render, screen } from '@testing-library/react';
+import { GifItem } from '../src/components/GifItem';
+
+describe('pruebas en <GifItem />', () => {
+
+    const title = 'GOT';
+    const url = 'https://got.com/got.jpg';
+
+    test('debe de hacer match con el snapshot', () => {
+        const { container } = render(<GifItem title={title} url={url} />);
+        expect(container).toMatchSnapshot();
+    });
+
+    test('debe de mostrar la imagen con el URL y el ALT indicado', () => {
+        render(<GifItem title={title} url={url} />);
+
+        const { src, alt } = screen.getByRole('img');
+
+        expect(src).toBe(url);
+        expect(alt).toBe(title);
+    });
+
+    test('debe de mostrar el titulo en el componente', () => {
+        render(<GifItem title={title} url={url} />);
+        expect(screen.getByText(title)).toBeTruthy();
+    });
+
+})
